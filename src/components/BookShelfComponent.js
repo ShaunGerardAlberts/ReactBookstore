df570aec158d3f86aa98a8777e03e4c6c22e51be
Shaun <alberts.shaun@gmail.com>
2017-12-20 20:51:29 +1000

    feat: set default bookurl if book doesnt have one

--- src/components/BookShelfComponent.js
+++ src/components/BookShelfComponent.js
@@ -25,32 +25,40 @@ class BookShelfComponent extends Component {
 
     render() {
         const { displayBooks, title } = this.props
+        var bookURL
         return (
             <div className="bookshelf">
                 <h2 className="bookshelf-title">{ title }</h2>
                 <div className="bookshelf-books">
                 <ol className="books-grid">
-                {displayBooks.map((book) => (
-                    <li key={ book.id }>
-                        <div className="book">
-                        <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, 
-                                backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
-                            <div className="book-shelf-changer">
-                                <select id={book.id} value={book.shelf} onChange={() => this.handleShelfChange(book)}>
-                                    <option value="none" disabled>Move to...</option>
-                                    <option value="currentlyReading">Currently Reading</option>
-                                    <option value="wantToRead">Want to Read</option>
-                                    <option value="read">Read</option>
-                                    <option value="none">None</option>
-                                </select>
+                {displayBooks.map((book) => {
+                    if (book.imageLinks.thumbnail === 'undefined') {
+                        bookURL = 'http://books.google.com/books/content?id=nggnmAEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api'
+                    } else {
+                        bookURL = book.imageLinks.thumbnail
+                    }
+                    return (
+                        <li key={ book.id }>
+                            <div className="book">
+                            <div className="book-top">
+                                <div className="book-cover" style={{ width: 128, height: 193, 
+                                    backgroundImage: `url(${bookURL})`}}></div>
+                                <div className="book-shelf-changer">
+                                    <select id={book.id} value={book.shelf} onChange={() => this.handleShelfChange(book)}>
+                                        <option value="none" disabled>Move to...</option>
+                                        <option value="currentlyReading">Currently Reading</option>
+                                        <option value="wantToRead">Want to Read</option>
+                                        <option value="read">Read</option>
+                                        <option value="none">None</option>
+                                    </select>
+                                </div>
                             </div>
-                        </div>
-                        <div className="book-title">{ book.title }</div>
-                        <div className="book-authors">{ book.authors }</div>
-                        </div>
-                    </li>
-                ))}
+                            <div className="book-title">{ book.title }</div>
+                            <div className="book-authors">{ book.authors }</div>
+                            </div>
+                        </li>
+                    )
+                })}
                 </ol>
             </div>
         </div>
--- src/components/ShelvesComponent.js
+++ src/components/ShelvesComponent.js
@@ -15,36 +15,6 @@ class ShelvesComponent extends Component {
         }
     }
 
-    renderShelf = (booksArray) => {
-        return (
-            <div className="bookshelf-books">
-            <ol className="books-grid">
-            {booksArray.map((book) => (
-                <li key={ book.id }>
-                    <div className="book">
-                    <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 193, 
-                            backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
-                        <div className="book-shelf-changer">
-                            <select id={book.id} value={book.shelf} onChange={() => this.handleShelfChange(book)}>
-                                <option value="none" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div className="book-title">{ book.title }</div>
-                    <div className="book-authors">{ book.authors }</div>
-                    </div>
-                </li>
-            ))}
-            </ol>
-        </div>
-        )
-    }
-
     render() {
         const { books } = this.props
         const shelves = [
